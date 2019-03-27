b699077833b2b9c3eab69c6f9140badcadadaea7
Pascal Vogler <pascalvogler@gmx.ch>
2019-03-27 08:22:27 +0100

    finish for today

--- base.py
+++ base.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import time
+from tqdm import tqdm, trange
 
 #df = pd.read_csv("https://docs.google.com/spreadsheets/u/0/d/1x0EzBMIOnFpyCPbO_vfW3ZZxFEfQyr5u7avzaCVMPXs/export?format=csv")
 
@@ -21,12 +22,11 @@ while True:
 print("I see, I see.... {} poor souls decided to enter the evil realm and the home of foul creatures. Beware, for your life is in great danger.".format(nrPlayers))
 print("Now, each of you has to introduce himself with his name and class. The following classes are available:")
 
-time.sleep(2)
+time.sleep(1.5)
 
 print("---")
 for item in availableClasses:
     print(item)
-
 print("---")
 
 
@@ -41,12 +41,18 @@ for i in range(nrPlayers):
             time.sleep(0.5)
             trash_class = input("Now tell me, peasant, what class do you wish to play?: ".format(trash_name))
     PlayerDict.update({i + 1 : Player(trash_name, trash_class)})
-    time.sleep(0.5)
-    print("So {} it is! Next Adventurer!".format(trash_class))
+    if i + 1 != nrPlayers:
+        time.sleep(0.5)
+        print("So {} it is! Next Adventurer!".format(trash_class))
+    else:
+        time.sleep(0.5)
+        print("So {} it is!".format(trash_class))
 
 time.sleep(0.5)
-print("...")
-time.sleep(0.5)
+
+print("")
 print("Hurraaaaay, the group is assembled. Here is a summary:")
+
 for key, value in PlayerDict.items():
     print("Player {}: {} as a {}".format(key,value.name, value.heroClass))
+
